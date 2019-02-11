eff03f700a7a3cf1d0aef414d17cef2db5ea6027
Димитрова Жени <jeninidi@Botond-iMac.local>
2019-02-11 12:30:20 +0100

    routing setup

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { WinebottlesListComponent } from './winebottles-list/winebottles-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '' ,
+    redirectTo:'/collection',
+    pathMatch:'full'
+  },
+  {
+    path: 'collection',
+    component: WinebottlesListComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
